f9dcc3a4936a87849103c2f614062a9726e31d64
erickzns <162044386+erickzns@users.noreply.github.com>
2025-02-03 20:19:09 -0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,4 +1,4 @@
--- Mod Menu Completo Fictício para Roblox
+-- Mod Menu Simplificado com uma Função (Speed Hack)
 
 -- Classe Base
 CheatFunction = {}
@@ -44,118 +44,6 @@ end
 
 print("Speed Hack criado")
 
--- Classe Super Salto (Super Jump)
-SuperJump = CheatFunction:new("Super Jump")
-
-function SuperJump:setJumpPower(value)
-    local player = game.Players.LocalPlayer
-    player.Character.Humanoid.JumpPower = value
-end
-
-function SuperJump:activate(value)
-    CheatFunction.activate(self)
-    self:setJumpPower(value)
-    print(self.name .. " activated with jump power " .. value)
-end
-
-function SuperJump:deactivate()
-    CheatFunction.deactivate(self)
-    self:setJumpPower(50) -- Valor padrão de salto
-end
-
-print("Super Jump criado")
-
--- Classe Gravidade Zero (Low Gravity)
-LowGravity = CheatFunction:new("Low Gravity")
-
-function LowGravity:setGravity(value)
-    game.Workspace.Gravity = value
-end
-
-function LowGravity:activate(value)
-    CheatFunction.activate(self)
-    self:setGravity(value)
-    print(self.name .. " activated with gravity " .. value)
-end
-
-function LowGravity:deactivate()
-    CheatFunction.deactivate(self)
-    self:setGravity(196.2) -- Valor padrão de gravidade
-end
-
-print("Low Gravity criado")
-
--- Classe Invencibilidade (God Mode)
-GodMode = CheatFunction:new("God Mode")
-
-function GodMode:activate()
-    CheatFunction.activate(self)
-    local player = game.Players.LocalPlayer
-    player.Character.Humanoid.MaxHealth = math.huge
-    player.Character.Humanoid.Health = math.huge
-end
-
-function GodMode:deactivate()
-    CheatFunction.deactivate(self)
-    local player = game.Players.LocalPlayer
-    player.Character.Humanoid.MaxHealth = 100
-    player.Character.Humanoid.Health = 100
-end
-
-print("God Mode criado")
-
--- Classe Voo (Fly Mode)
-FlyMode = CheatFunction:new("Fly Mode")
-
-function FlyMode:fly(value)
-    -- Função fictícia para voo
-end
-
-function FlyMode:activate(value)
-    CheatFunction.activate(self)
-    self:fly(value)
-    print(self.name .. " activated with fly speed " .. value)
-end
-
-function FlyMode:deactivate()
-    CheatFunction.deactivate(self)
-    self:fly(0) -- Desativar voo
-end
-
-print("Fly Mode criado")
-
--- Classe Teleportação (Teleportation)
-Teleportation = CheatFunction:new("Teleportation")
-
-function Teleportation:teleportToPlayer(playerName)
-    local player = game.Players.LocalPlayer
-    local targetPlayer = game.Players:FindFirstChild(playerName)
-    if targetPlayer then
-        player.Character:SetPrimaryPartCFrame(targetPlayer.Character:GetPrimaryPartCFrame())
-    end
-end
-
-function Teleportation:teleportToCoords(x, y, z)
-    local player = game.Players.LocalPlayer
-    player.Character:SetPrimaryPartCFrame(CFrame.new(x, y, z))
-end
-
-function Teleportation:activate(targetType, target)
-    CheatFunction.activate(self)
-    if targetType == "player" then
-        self:teleportToPlayer(target)
-    else
-        self:teleportToCoords(target.x, target.y, target.z)
-    end
-end
-
-function Teleportation:deactivate()
-    CheatFunction.deactivate(self)
-    print(self.name .. " deactivated.")
-end
-
-print("Teleportation criado")
-
 -- Classe Menu Principal
 CheatMenu = {}
 CheatMenu.__index = CheatMenu
@@ -188,41 +76,17 @@ end
 
 print("Menu principal criado")
 
--- Criando instâncias das funções de cheats
+-- Criando instância da função de Speed Hack
 local speedHack = SpeedHack:new()
-local superJump = SuperJump:new()
-local lowGravity = LowGravity:new()
-local godMode = GodMode:new()
-local flyMode = FlyMode:new()
-local teleportation = Teleportation:new()
 
--- Criando o menu principal e adicionando as funções de cheats
+-- Criando o menu principal e adicionando a função de Speed Hack
 local cheatMenu = CheatMenu:new()
 cheatMenu:addFunction(speedHack)
-cheatMenu:addFunction(superJump)
-cheatMenu:addFunction(lowGravity)
-cheatMenu:addFunction(godMode)
-cheatMenu:addFunction(flyMode)
-cheatMenu:addFunction(teleportation)
 
-print("Funções adicionadas ao menu")
+print("Função adicionada ao menu")
 
--- Exemplos de ativação e desativação das funções
+-- Exemplos de ativação e desativação da função de Speed Hack
 cheatMenu:activateFunction("Speed Hack", 10)
-cheatMenu:activateFunction("Super Jump", 100)
-cheatMenu:activateFunction("Low Gravity", 50)
-cheatMenu:activateFunction("God Mode")
-cheatMenu:activateFunction("Fly Mode", 50)
-cheatMenu:activateFunction("Teleportation", {x = 0, y = 10, z = 0})
-
-print("Funções ativadas")
-
--- Desativação
+print("Função Speed Hack ativada")
 cheatMenu:deactivateFunction("Speed Hack")
-cheatMenu:deactivateFunction("Super Jump")
-cheatMenu:deactivateFunction("Low Gravity")
-cheatMenu:deactivateFunction("God Mode")
-cheatMenu:deactivateFunction("Fly Mode")
-cheatMenu:deactivateFunction("Teleportation")
-
-print("Funções desativadas")
+print("Função Speed Hack desativada")
