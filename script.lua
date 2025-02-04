78df6e2bf4c1263ad0958246fd1c856f442c25fb
erickzns <162044386+erickzns@users.noreply.github.com>
2025-02-04 18:53:40 -0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -94,8 +94,8 @@ local function addCheckboxToMenu(functionName)
 
     -- Texto da função
     local CheckBoxText = Instance.new("TextLabel")
-    CheckBoxText.Size = UDim2.new(0, 340, 0, 50)
-    CheckBoxText.Position = UDim2.new(0, 10, 0, 0)
+    CheckBoxText.Size = UDim2.new(0, 300, 0, 50)  -- Ajustado para caber o texto
+    CheckBoxText.Position = UDim2.new(0, 10, 0, 0)  -- Posiciona o texto do nome da função
     CheckBoxText.Text = functionName
     CheckBoxText.TextColor3 = Color3.fromRGB(255, 255, 255)
     CheckBoxText.BackgroundTransparency = 1
